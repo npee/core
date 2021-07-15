498f4ddaa5ac9a44bd41825e4bcb452438e0846e
npee <jmc4664@gmail.com>
2021-07-15 23:55:35 +0900

    JUnit 사용하여 주문 구현체 테스트

---
+++ src/test/java/io/npee/core/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package io.npee.core.order;
+
+import io.npee.core.member.Grade;
+import io.npee.core.member.Member;
+import io.npee.core.member.MemberService;
+import io.npee.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
