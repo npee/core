a005b6bd763af2ff9624ae4d9d234159b3d9cca0
npee <jmc4664@gmail.com>
2021-07-16 20:51:52 +0900

    기존 할인 인터페이스를 구현한 정품 할인 정책 구현체 작성

---
+++ src/main/java/io/npee/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package io.npee.core.discount;
+
+import io.npee.core.member.Grade;
+import io.npee.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
