53d8203a5e1a5b3bc8677d4747ccac33fd9735de
npee <jmc4664@gmail.com>
2021-07-17 19:33:19 +0900

    JUnit 사용하여 정품할인 정책 구현체 테스트

---
+++ src/test/java/io/npee/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,28 @@
+package io.npee.core.discount;
+
+import io.npee.core.member.Grade;
+import io.npee.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    void vip_o() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+}
