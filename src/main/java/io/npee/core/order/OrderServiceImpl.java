85a5db061d82d61a3e8cd83fc7eeb014939b570f
npee <jmc4664@gmail.com>
2021-07-19 22:21:26 +0900

    생성자 구현체 부분 변경 -> DIP, OCP 위반하는 상황

--- src/main/java/io/npee/core/order/OrderServiceImpl.java
+++ src/main/java/io/npee/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package io.npee.core.order;
 
 import io.npee.core.discount.DiscountPolicy;
 import io.npee.core.discount.FixDiscountPolicy;
+import io.npee.core.discount.RateDiscountPolicy;
 import io.npee.core.member.Member;
 import io.npee.core.member.MemberRepository;
 import io.npee.core.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import io.npee.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();   // 구현체 의존(DIP 위반)
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();     // 구현체의 코드를 고쳐야함(OCP 위반)
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
