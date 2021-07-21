a3980579a1bf85871243dae93368723856bc0587
npee <jmc4664@gmail.com>
2021-07-21 22:15:20 +0900

    현재 상태로는 DIP를 지킬 수 없음

--- src/main/java/io/npee/core/order/OrderServiceImpl.java
+++ src/main/java/io/npee/core/order/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package io.npee.core.order;
 
 import io.npee.core.discount.DiscountPolicy;
+import io.npee.core.discount.FixDiscountPolicy;
 import io.npee.core.member.Member;
 import io.npee.core.member.MemberRepository;
 import io.npee.core.member.MemoryMemberRepository;
@@ -8,7 +9,7 @@ import io.npee.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private DiscountPolicy discountPolicy;  // 누군가 대신 주입해줘야 한다
+    private DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
