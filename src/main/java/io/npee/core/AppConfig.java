d9db39bb42c100c19af0afb27a1b6d28e8f81c06
npee <jmc4664@gmail.com>
2021-07-23 23:03:13 +0900

    MemberService 를 호출 할 때 AppConfig에서 생성자를 통하여 생성하도록 작성

--- src/main/java/io/npee/core/AppConfig.java
+++ src/main/java/io/npee/core/AppConfig.java
@@ -2,10 +2,11 @@ package io.npee.core;
 
 import io.npee.core.member.MemberService;
 import io.npee.core.member.MemberServiceImpl;
+import io.npee.core.member.MemoryMemberRepository;
 
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl();
+        return new MemberServiceImpl(new MemoryMemberRepository());
     }
 }
