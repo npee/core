732d997fca2f1abed79fd0dbe42d6c758c58836e
npee <jmc4664@gmail.com>
2021-07-24 20:05:10 +0900

    OrderService 도 AppConfig의 생성자를 이용하도록 수정

--- src/main/java/io/npee/core/AppConfig.java
+++ src/main/java/io/npee/core/AppConfig.java
@@ -1,12 +1,20 @@
 package io.npee.core;
 
+import io.npee.core.discount.FixDiscountPolicy;
+import io.npee.core.member.MemberRepository;
 import io.npee.core.member.MemberService;
 import io.npee.core.member.MemberServiceImpl;
 import io.npee.core.member.MemoryMemberRepository;
+import io.npee.core.order.OrderService;
+import io.npee.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new  MemoryMemberRepository(), new FixDiscountPolicy());
     }
 }
