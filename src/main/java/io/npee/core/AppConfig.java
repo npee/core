a30876876976e1d6bf7da0c3f6aaa0decebde03b
npee <jmc4664@gmail.com>
2021-07-27 21:31:31 +0900

    리팩토링

--- src/main/java/io/npee/core/AppConfig.java
+++ src/main/java/io/npee/core/AppConfig.java
@@ -11,10 +11,18 @@ import io.npee.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new  MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
