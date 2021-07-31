2a4019416f71fb0a8e805cd7af3314799baf300d
npee <jmc4664@gmail.com>
2021-07-31 13:50:05 +0900

    스프링 빈 출력 테스트 - 모든 빈

---
+++ src/test/java/io/npee/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,21 @@
+package io.npee.core.beanfind;
+
+import io.npee.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("bean = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+}
