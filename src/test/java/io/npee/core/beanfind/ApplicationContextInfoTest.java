af586a93671986013a2a31c62bd9c4c5ac14b513
npee <jmc4664@gmail.com>
2021-08-01 20:00:03 +0900

    스프링 빈 출력 테스트 - 애플리케이션 빈

--- src/test/java/io/npee/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/io/npee/core/beanfind/ApplicationContextInfoTest.java
@@ -3,6 +3,7 @@ package io.npee.core.beanfind;
 import io.npee.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContextInfoTest {
@@ -16,6 +17,20 @@ public class ApplicationContextInfoTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
             System.out.println("bean = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("bean = " + beanDefinitionName + " object = " + bean);
+            }
         }
     }
 }
