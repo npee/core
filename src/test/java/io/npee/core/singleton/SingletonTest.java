393390fdea9330107b23ff87b79aee3524d95125
npee <jmc4664@gmail.com>
2021-08-08 13:34:40 +0900

    싱글톤 패턴 없이 사용자 요청이 있는 경우 객체 생성 테스트

---
+++ src/test/java/io/npee/core/singleton/SingletonTest.java
@@ -0,0 +1,26 @@
+package io.npee.core.singleton;
+
+import io.npee.core.AppConfig;
+import io.npee.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        // 1. 조회: 호출 할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+        // 2. 조회: 호출 할 떄 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
