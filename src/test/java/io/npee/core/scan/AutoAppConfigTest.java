b374b4c7a7cbec9618445d72ee3a5472c21833f4
npee <jmc4664@gmail.com>
2021-08-22 22:00:48 +0900

    필드 주입(권장하지 않음) - 필드에 직접 @Autowired 붙여서 동작하는 것 확인 테스트

--- src/test/java/io/npee/core/scan/AutoAppConfigTest.java
+++ src/test/java/io/npee/core/scan/AutoAppConfigTest.java
@@ -1,8 +1,10 @@
 package io.npee.core.scan;
 
 import io.npee.core.AutoAppConfig;
+import io.npee.core.member.MemberRepository;
 import io.npee.core.member.MemberService;
 import io.npee.core.order.OrderService;
+import io.npee.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -16,5 +18,9 @@ public class AutoAppConfigTest {
         MemberService memberService = ac.getBean(MemberService.class);
 
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
