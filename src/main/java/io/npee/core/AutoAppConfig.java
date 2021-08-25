4235bb50f3e4979c316a7aac08fbbc622052be42
npee <jmc4664@gmail.com>
2021-08-25 10:00:04 +0900

    일반 메서드 주입 - 수정자 주입과 유사함

--- src/main/java/io/npee/core/AutoAppConfig.java
+++ src/main/java/io/npee/core/AutoAppConfig.java
@@ -19,12 +19,4 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
-    @Autowired MemberRepository memberRepository;
-    @Autowired DiscountPolicy discountPolicy;
-
-    @Bean
-    OrderService orderService() {
-        return new OrderServiceImpl(memberRepository, discountPolicy);
-    }
 }
