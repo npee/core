bf8daa022d5b08016ed4b624287719675d08ccbd
npee <jmc4664@gmail.com>
2021-08-27 10:15:20 +0900

    생성자 주입의 이점 - 자바 코드 수준에서 테스트 가능

---
+++ src/test/java/io/npee/core/order/OrderServiceImplTest.java
@@ -0,0 +1,22 @@
+package io.npee.core.order;
+
+import io.npee.core.discount.FixDiscountPolicy;
+import io.npee.core.member.Grade;
+import io.npee.core.member.Member;
+import io.npee.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+        memoryMemberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memoryMemberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
