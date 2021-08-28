aeffdc2956d10031d92cd3b8a08fae943afb2b1d
npee <jmc4664@gmail.com>
2021-08-28 22:26:14 +0900

    롬복 사용 - 데이터, 서비스 로직에 적용

--- src/main/java/io/npee/core/order/OrderServiceImpl.java
+++ src/main/java/io/npee/core/order/OrderServiceImpl.java
@@ -5,19 +5,16 @@ import io.npee.core.discount.FixDiscountPolicy;
 import io.npee.core.member.Member;
 import io.npee.core.member.MemberRepository;
 import io.npee.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
