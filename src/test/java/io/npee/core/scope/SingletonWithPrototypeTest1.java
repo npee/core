51255d83551714316a84a6c987e45102f9351b04
npee <jmc4664@gmail.com>
2021-09-04 10:29:19 +0900

    프로토타입, 싱글톤 빈 동시 사용시 문제 - DL 기능만 가지는 더 단순한 Provider 사용(라이브러리 필요)

--- src/test/java/io/npee/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/io/npee/core/scope/SingletonWithPrototypeTest1.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -41,10 +42,10 @@ public class SingletonWithPrototypeTest1 {
     static class ClientBean {
 
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             System.out.println("prototypeBean " + prototypeBean);
             int count = prototypeBean.getCount();
